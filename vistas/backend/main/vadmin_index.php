67ce283c930164734cf7e7fed51184036a6acf22
Iván <sangar1982@gmail.com>
2011-02-16 19:26:40 +0100

    Validacion js para tipos image y file mejorada

--- vistas/backend/main/vadmin_index.php
+++ vistas/backend/main/vadmin_index.php
@@ -4,8 +4,7 @@ echo "<h1>Zona privada</h1>";
 
 echo "<ul id='privatezone'>";
   echo "<li><a href='/admin/users/list/'>Gestión de usuarios</a></li>";
-	echo "<li><a href='/admin/categoria/list/'>Gestión de categoria</a></li>";
-//#NO-BORRAR#//
+	//#NO-BORRAR#//
 echo "</ul>";
 
 ?>
