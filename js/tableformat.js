be45de80f6d63702db67527fac9fdd55409b09f1
Iván <isanchez@sinermedia.com>
2011-10-24 10:06:35 +0200

    Arreglado error en table format

--- js/tableformat.js
+++ js/tableformat.js
@@ -1,5 +1,5 @@
 
 	$(document).ready(function(){
-	        $("tr:odd").css("background", "#ECF2F6"); // filas impares
-	        $("tr:even").css("background", "#fff"); // filas pares
+	        $(".formattable tr:odd").css("background", "#ECF2F6"); // filas impares
+	        $(".formattable tr:even").css("background", "#fff"); // filas pares
 	 });
